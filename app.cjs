cac2e9be45534b4f8a6d6b6137af6b053f464dc7
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-17 10:32:37 +0100

    chore: set up EJS view engine and Express EJS layouts

--- app.cjs
+++ app.cjs
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("node:path");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 8080;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(expressLayouts);
 
 app.listen(port, (error) => {
   if (error) {
