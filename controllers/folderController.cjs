914ab92a75a854724c593fed7f0d843fa3294d3e
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-22 12:14:55 +0100

    feat: add renderCreateFolder handler to render create folder view

--- controllers/folderController.cjs
+++ controllers/folderController.cjs
@@ -1,8 +1,12 @@
 const folderRepository = require("../lib/repositories/folder.repository.js");
 
+function renderCreateFolder(req, res) {
+  res.render("createFolder", { title: "New Folder" });
+}
+
 async function renderManageFolders(req, res) {
   const folders = await folderRepository.getAllFolders();
   res.render("manageFolders", { title: "Manage Folders", folders: folders });
 }
 
-module.exports = { renderManageFolders };
+module.exports = { renderCreateFolder, renderManageFolders };
