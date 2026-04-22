57e826d097e8d7600cd1734d733c3e283a055978
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-22 12:07:01 +0100

    feat: add GET route for manage path

--- routes/folderRouter.cjs
+++ routes/folderRouter.cjs
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const folderRouter = Router();
+const folderController = require("../controllers/folderController.cjs");
 const fileController = require("../controllers/fileController.cjs");
 
+folderRouter.get("/manage", folderController.renderManageFolders);
 folderRouter.get("/:folderId", fileController.getFilesByFolderId);
 
 module.exports = folderRouter;
