531267e93e513a42054217c414f6a110a7e9037c
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-22 16:03:41 +0100

    feat: add GET, POST routes for folder create path

--- routes/folderRouter.cjs
+++ routes/folderRouter.cjs
@@ -3,7 +3,10 @@ const folderRouter = Router();
 const folderController = require("../controllers/folderController.cjs");
 const fileController = require("../controllers/fileController.cjs");
 
+folderRouter.get("/create", folderController.renderCreateFolder);
 folderRouter.get("/manage", folderController.renderManageFolders);
 folderRouter.get("/:folderId", fileController.getFilesByFolderId);
+
+folderRouter.post("/create", folderController.folderValidators, folderController.createFolder);
 
 module.exports = folderRouter;
