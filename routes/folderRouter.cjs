8eb6f1e2a2e046d362702b42cafbf9ace8278994
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-22 19:40:36 +0100

    feat: add GET, POST routes for edit folder paths

--- routes/folderRouter.cjs
+++ routes/folderRouter.cjs
@@ -6,7 +6,9 @@ const fileController = require("../controllers/fileController.cjs");
 folderRouter.get("/create", folderController.renderCreateFolder);
 folderRouter.get("/manage", folderController.renderManageFolders);
 folderRouter.get("/:folderId", fileController.getFilesByFolderId);
+folderRouter.get("/edit/:folderId", folderController.renderEditFolder);
 
 folderRouter.post("/create", folderController.folderValidators, folderController.createFolder);
+folderRouter.post("/edit/:folderId", folderController.folderValidators, folderController.updateFolder);
 
 module.exports = folderRouter;
