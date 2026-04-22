7c76a294260b945cfd88a62e2a2b8273f0ad5459
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-22 11:09:09 +0100

    refactor: getAllFolders from database and pass to folders prop
    - getAllFolders will render form select options for upload file form

--- controllers/indexController.cjs
+++ controllers/indexController.cjs
@@ -1,5 +1,8 @@
-function getHomepage(req, res) {
-  res.render("index", { title: "Upload File" });
+const folderRepository = require("../lib/repositories/folder.repository.js");
+
+async function getHomepage(req, res) {
+  const folders = await folderRepository.getAllFolders();
+  res.render("index", { title: "Upload File", folders: folders });
 }
 
 module.exports = { getHomepage };
