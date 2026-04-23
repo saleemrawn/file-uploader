5f511bf80ea4968f63e4052a369b33dc6604cc64
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-23 16:16:12 +0100

    feat: add GET route for file upload path

--- routes/fileRouter.cjs
+++ routes/fileRouter.cjs
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const fileRouter = Router();
 const fileController = require("../controllers/fileController.cjs");
 
+fileRouter.get("/upload", fileController.renderUploadFile);
 fileRouter.post("/upload", fileController.uploadFile);
 
 module.exports = fileRouter;
