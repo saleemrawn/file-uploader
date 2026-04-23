3754a2dfbd2bc89579d9d5f662c5569dd7ff7e54
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-23 16:05:32 +0100

    refactor: replace folder listing with recent files on homepage

--- controllers/indexController.cjs
+++ controllers/indexController.cjs
@@ -2,8 +2,8 @@ const folderRepository = require("../lib/repositories/folder.repository.js");
 const fileRepository = require("../lib/repositories/file.repository.js");
 
 async function getHomepage(req, res) {
-  const folders = await folderRepository.getAllFolders();
-  res.render("index", { title: "Welcome", folders: folders });
+  const files = await fileRepository.getAllFiles();
+  res.render("index", { title: "Welcome", files: files.slice(0, 5) });
 }
 
 async function renderRecentFiles(req, res) {
