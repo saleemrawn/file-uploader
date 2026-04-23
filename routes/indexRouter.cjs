5246f5309de76ef285eb3ff16dc148e9b9645941
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-23 12:13:47 +0100

    feat: add GET route for recent files path

--- routes/indexRouter.cjs
+++ routes/indexRouter.cjs
@@ -3,5 +3,6 @@ const indexController = require("../controllers/indexController.cjs");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getHomepage);
+indexRouter.get("/recent", indexController.renderRecentFiles);
 
 module.exports = indexRouter;
