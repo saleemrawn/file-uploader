4d89fa22487fbbfd61a8504947107bd4d9ab8137
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-23 12:13:06 +0100

    feat: add renderRecentFiles handle to render recent files view

--- controllers/indexController.cjs
+++ controllers/indexController.cjs
@@ -1,8 +1,14 @@
 const folderRepository = require("../lib/repositories/folder.repository.js");
+const fileRepository = require("../lib/repositories/file.repository.js");
 
 async function getHomepage(req, res) {
   const folders = await folderRepository.getAllFolders();
   res.render("index", { title: "Upload File", folders: folders });
 }
 
-module.exports = { getHomepage };
+async function renderRecentFiles(req, res) {
+  const files = await fileRepository.getAllFiles();
+  res.render("recent", { title: "Recent", files: files });
+}
+
+module.exports = { getHomepage, renderRecentFiles };
