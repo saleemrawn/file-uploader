1281ab24b62b447712f83dcd2ab0f8f8a11bc878
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-27 15:48:11 +0100

    refactor: update database query in renderRecentFiles handler
    - update database query in renderRecentFiles to getAllFilesByOwnerId to only fetch files for logged in user
    - pass ownerId to getAllFilesByOwnerId

--- controllers/indexController.cjs
+++ controllers/indexController.cjs
@@ -2,12 +2,13 @@ const folderRepository = require("../lib/repositories/folder.repository.js");
 const fileRepository = require("../lib/repositories/file.repository.js");
 
 async function getHomepage(req, res) {
-  const files = await fileRepository.getAllFiles();
+  const files = await fileRepository.getAllFilesByOwnerId();
   res.render("index", { title: "Welcome", files: files.slice(0, 5) });
 }
 
 async function renderRecentFiles(req, res) {
-  const files = await fileRepository.getAllFiles();
+  const ownerId = Number(req.user.id);
+  const files = await fileRepository.getAllFilesByOwnerId(ownerId);
   res.render("recent", { title: "Recent", files: files });
 }
 
