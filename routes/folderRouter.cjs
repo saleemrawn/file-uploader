29eb0953698548fab1a5cda40209efb3cfbb4646
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-28 14:06:28 +0100

    refactor: replace folderId route handler with renderFolder

--- routes/folderRouter.cjs
+++ routes/folderRouter.cjs
@@ -1,11 +1,10 @@
 const { Router } = require("express");
 const folderRouter = Router();
 const folderController = require("../controllers/folderController.cjs");
-const fileController = require("../controllers/fileController.cjs");
 
 folderRouter.get("/create", folderController.renderCreateFolder);
 folderRouter.get("/manage", folderController.renderManageFolders);
-folderRouter.get("/:folderId", fileController.getFilesByFolderId);
+folderRouter.get("/:folderId", folderController.renderFolder);
 folderRouter.get("/edit/:folderId", folderController.renderEditFolder);
 
 folderRouter.post("/create", folderController.folderValidators, folderController.createFolder);
