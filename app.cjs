04059589a5eaba7662a8855d00e2888d5c88c11a
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-07 15:56:36 +0100

    refactor: move session secret to .env

--- app.cjs
+++ app.cjs
@@ -22,7 +22,7 @@ app.use(
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // ms
     },
-    secret: "a santa at nasa",
+    secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     store: new PrismaSessionStore(prisma, {
