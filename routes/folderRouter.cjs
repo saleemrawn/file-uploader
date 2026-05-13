62e75312c0be6b05a9564f82deb58e182c317202
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-13 10:01:20 +0100

    feat: add GET, POST routes for folder link sharing
    - add POST route /share path for folder share link generating
    - add GET route /share/:folderId path to render shareFolder view
    - add GET route /shared/:shareUuid path to render folder for shared link

--- routes/folderRouter.cjs
+++ routes/folderRouter.cjs
@@ -6,8 +6,11 @@ folderRouter.get("/create", folderController.renderCreateFolder);
 folderRouter.get("/manage", folderController.renderManageFolders);
 folderRouter.get("/:folderId", folderController.renderFolder);
 folderRouter.get("/edit/:folderId", folderController.renderEditFolder);
+folderRouter.get("/share/:folderId", folderController.renderShareFolderForm);
+folderRouter.get("/shared/:shareUuid", folderController.renderSharedFolder);
 
 folderRouter.post("/create", folderController.folderValidators, folderController.createFolder);
+folderRouter.post("/share", folderController.shareFolder);
 folderRouter.post("/edit/:folderId", folderController.folderValidators, folderController.updateFolder);
 folderRouter.post("/delete/:folderId", folderController.deleteFolder);
 
